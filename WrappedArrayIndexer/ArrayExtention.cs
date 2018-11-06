9b90e30cbd03522c8708f2e693ed70b2a602b14f
Austin Gatto <austin.s.c.gatto@outlook.com>
2018-11-06 19:56:19 +1100

    init

---
+++ WrappedArrayIndexer/ArrayExtention.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WrappedArrayIndexer
+{
+    public static class ArrayExtention
+    {
+        public static T Wrap<T>(this Array array, params int[] indices)
+        {
+            if (indices.Length != array.Rank) throw new ArgumentOutOfRangeException(indices.ToString(), $"{indices.Length} indices provided, but Array rank is {array.Rank}. These values should match.");
+
+            int[] wrappedIndices = new int[indices.Length];
+            for (int i = 0; i < indices.Length; i++)
+            {
+                int index = indices[i];
+                int bound = array.GetLength(i);
+                Console.WriteLine(bound);
+                wrappedIndices[i] = ((index % bound) + bound) % bound;
+            }   
+            return (T)array.GetValue(wrappedIndices);
+        }
+    }
+}
